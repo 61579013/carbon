528538f9d4c0385f710bdd7ed9422b340280e6b1
gouguoyin <245629560@qq.com>
2021-04-06 15:00:40 +0800

    carbon实现String接口

--- carbon.go
+++ carbon.go
@@ -136,6 +136,14 @@ type Carbon struct {
 	Error error
 }
 
+// 实现String接口
+func (c Carbon) String() string {
+	if c.IsZero() {
+		return ""
+	}
+	return c.ToDateTimeString()
+}
+
 // Time2Carbon 将 time.Time 转换成 Carbon
 func Time2Carbon(tt time.Time) Carbon {
 	return Carbon{Time: tt, Loc: time.Local}
