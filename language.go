89e8cfbdc0b113529e13f502bcf712589644c875
gouguoyin <245629560@qq.com>
2021-08-23 11:13:22 +0800

    optimization logic

--- language.go
+++ language.go
@@ -4,7 +4,7 @@ import (
 	"strconv"
 	"strings"
 
-	"gitee.com/go-package/carbon/lang"
+	"gitee.com/golang-package/carbon/lang"
 )
 
 var (
@@ -67,8 +67,8 @@ func (l *Language) translate(unit string, number int64) string {
 	if len(slice) == 1 {
 		return strings.Replace(slice[0], "%d", strconv.FormatInt(number, 10), 1)
 	}
-	if number > 1 {
-		return strings.Replace(slice[1], "%d", strconv.FormatInt(number, 10), 1)
+	if int64(len(slice)) <= number {
+		return strings.Replace(slice[len(slice)-1], "%d", strconv.FormatInt(number, 10), 1)
 	}
-	return slice[0]
+	return strings.Replace(slice[number-1], "%d", strconv.FormatInt(number, 10), 1)
 }
