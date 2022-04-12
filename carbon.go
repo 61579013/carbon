de3f07f9c70d328fc932085228ff31eb8fdc48ba
gouguoyin <245629560@qq.com>
2022-04-12 23:18:18 +0800

    更新版本号，去掉无用的常量

--- carbon.go
+++ carbon.go
@@ -1,7 +1,7 @@
 // @Package carbon
 // @Description a simple, semantic and developer-friendly golang package for datetime
 // @Page github.com/golang-module/carbon
-// @Version v2.0.2
+// @Version v2.1.0
 // @Author gouguoyin
 // @Blog www.gouguoyin.cn
 // @Email contact@gouguoyin.cn
@@ -94,29 +94,26 @@ const (
 // numbers constant
 // 数字常量
 const (
-	YearsPerMillennium         = 1000    // 每千年1000年
-	YearsPerCentury            = 100     // 每世纪100年
-	YearsPerDecade             = 10      // 每十年10年
-	QuartersPerYear            = 4       // 每年4个季度
-	MonthsPerYear              = 12      // 每年12月
-	MonthsPerQuarter           = 3       // 每季度3月
-	WeeksPerNormalYear         = 52      // 每常规年52周
-	weeksPerLongYear           = 53      // 每长年53周
-	WeeksPerMonth              = 4       // 每月4周
-	DaysPerLeapYear            = 366     // 每闰年366天
-	DaysPerNormalYear          = 365     // 每常规年365天
-	DaysPerWeek                = 7       // 每周7天
-	HoursPerWeek               = 168     // 每周168小时
-	HoursPerDay                = 24      // 每天24小时
-	MinutesPerDay              = 1440    // 每天1440分钟
-	MinutesPerHour             = 60      // 每小时60分钟
-	SecondsPerWeek             = 604800  // 每周604800秒
-	SecondsPerDay              = 86400   // 每天86400秒
-	SecondsPerHour             = 3600    // 每小时3600秒
-	SecondsPerMinute           = 60      // 每分钟60秒
-	MillisecondsPerSecond      = 1000    // 每秒1000毫秒
-	MicrosecondsPerMillisecond = 1000    // 每毫秒1000微秒
-	MicrosecondsPerSecond      = 1000000 // 每秒1000000微秒
+	YearsPerMillennium = 1000   // 每千年1000年
+	YearsPerCentury    = 100    // 每世纪100年
+	YearsPerDecade     = 10     // 每十年10年
+	QuartersPerYear    = 4      // 每年4个季度
+	MonthsPerYear      = 12     // 每年12月
+	MonthsPerQuarter   = 3      // 每季度3月
+	WeeksPerNormalYear = 52     // 每常规年52周
+	weeksPerLongYear   = 53     // 每长年53周
+	WeeksPerMonth      = 4      // 每月4周
+	DaysPerLeapYear    = 366    // 每闰年366天
+	DaysPerNormalYear  = 365    // 每常规年365天
+	DaysPerWeek        = 7      // 每周7天
+	HoursPerWeek       = 168    // 每周168小时
+	HoursPerDay        = 24     // 每天24小时
+	MinutesPerDay      = 1440   // 每天1440分钟
+	MinutesPerHour     = 60     // 每小时60分钟
+	SecondsPerWeek     = 604800 // 每周604800秒
+	SecondsPerDay      = 86400  // 每天86400秒
+	SecondsPerHour     = 3600   // 每小时3600秒
+	SecondsPerMinute   = 60     // 每分钟60秒
 )
 
 // formats constant
